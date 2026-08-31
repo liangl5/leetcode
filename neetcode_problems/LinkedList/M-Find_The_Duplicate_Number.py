14aab6417510e725ef613352f52f5735bffb4aca
Luke Liang <liangl5@miamioh.edu>
2026-08-30 23:02:33 -0400

    linked list problems

--- neetcode_problems/LinkedList/M-Find_The_Duplicate_Number.py
+++ neetcode_problems/LinkedList/M-Find_The_Duplicate_Number.py
@@ -1,5 +1,26 @@
-# Hint:
+# Hint: the problem setup treats values an indices, allowing for cycle detection
+# First didn't quite understand how the problem relate to linked lists, it makese sense now. 
+# Cycle detection methodology only returns true or false if a cycle is detected, a second
+# passthrough is required to find actual duplicate value (i.e. the start of the cycle). 
+# Mathmatical formula is this: L (length of strip before cycle) + X (position in the cycle)
+# Since fast & slow ended on same, then extra traveled distance (L+X) must equal some k*C(length of cycle)
+# Now reset one of them, the other one is still on X. This means if you add L more steps, then both 
+# pointers will have traveled to the start of the cycle (L+X) and (L) 
 
+from typing import List
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        pass
+        # basically do a cycle detection method
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
--- neetcode_problems/LinkedList/M-LRU_Cache.py
+++ neetcode_problems/LinkedList/M-LRU_Cache.py
@@ -1,11 +1,74 @@
-# Hint:
+# Hint: use a doubly linked list to track order and a hashmap for easy lookup
+# Only thing I forgot was when accessing the hashmap, order should also be upated.
+
+class Node:
+    def __init__(self, key=None, val=None, next=None, prev=None):
+        self.key = key
+        self.val = val
+        self.next = next
+        self.prev = prev
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        pass
+        # stack + hashmap?
+        self.nums = 0
+        self.cap = capacity
+        self.hashing = {}
+
+        self.list_start = Node(val=None)
+        self.list_end = Node(val=None)
+        self.list_start.next = self.list_end
+        self.list_end.prev = self.list_start
 
     def get(self, key: int) -> int:
-        pass
+        if key in self.hashing:
+            node = self.hashing[key]
+            # move to front
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            
+            s_old = self.list_start.next
+            self.list_start.next = node
+            node.prev = self.list_start
+            node.next = s_old
+            s_old.prev = node
+            return node.val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        pass
+        if key in self.hashing:
+            # in cache so move existing to front
+            self.hashing[key].val = value
+
+            p = self.hashing[key].prev
+            n = self.hashing[key].next
+            p.next = n
+            n.prev = p
+
+            s_old = self.list_start.next
+            self.list_start.next = self.hashing[key]
+            self.hashing[key].prev = self.list_start
+            self.hashing[key].next = s_old
+            s_old.prev = self.hashing[key]
+            return
+
+        if self.nums >= self.cap:
+            # remove end one for space
+            node_to_del = self.list_end.prev
+            end_new = node_to_del.prev
+            del self.hashing[node_to_del.key]
+            del node_to_del
+            self.nums -= 1
+
+            end_new.next = self.list_end
+            self.list_end.prev = end_new
+        
+        # there is enough space, populate to the start
+        s_old = self.list_start.next
+        new = Node(key, value, next=s_old, prev=self.list_start)
+        self.hashing[key] = new
+        self.nums += 1
+
+        self.list_start.next = new
+        s_old.prev = new
