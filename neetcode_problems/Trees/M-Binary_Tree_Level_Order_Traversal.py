c9dcc72cc11f11e4c9d23b8bbde4b86cd9464485
Luke Liang <liangl5@miamioh.edu>
2026-09-13 17:39:57 -0400

    trees

--- neetcode_problems/Trees/M-Binary_Tree_Level_Order_Traversal.py
+++ neetcode_problems/Trees/M-Binary_Tree_Level_Order_Traversal.py
@@ -1,7 +1,34 @@
-# Hint:
+# Hint: BFS + tracking
+# fairly simple
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 from collections import deque
-
+from typing import List, Optional
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        pass
+        if not root:
+            return []
+
+        queue = deque([root])
+        res = []
+
+        while queue:
+            l = len(queue)
+            tmp = []
+            for _ in range(l):
+                curr = queue.popleft()
+                tmp.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+
+            res.append(tmp)
+
+        return res
--- neetcode_problems/Trees/M-Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ neetcode_problems/Trees/M-Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -1,5 +1,32 @@
-# Hint:
+# Hint: use BST properties
+# Didn't use BST properties, left artifacts of a solution for traversing a 
+# tree normally (queue), can also simplify conditions
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pass
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        curr = root
+        
+        if p.val < q.val:
+            p, q = q, p
+        # q is smallest, p is largest
+
+        while curr:
+            # if curr.val > q.val and curr.val < p.val:
+            #     return curr
+            # elif curr.val == p.val or curr.val == q.val:
+            #     return curr
+                
+            if curr.val < q.val:
+                curr = curr.right
+            elif curr.val > p.val:
+                curr = curr.left
+            else:
+                return curr
+
