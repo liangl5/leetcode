d6a674539f9045f7586628a46f3dfe3c7a7b02c1
Luke Liang <liangl5@miamioh.edu>
2026-09-14 12:15:39 -0400

    1D - DP

--- neetcode_problems/1D_DynamicProgramming/M-Coin_Change.py
+++ neetcode_problems/1D_DynamicProgramming/M-Coin_Change.py
@@ -1,5 +1,32 @@
-# Hint:
+# Hint: started from amount then DP backwards
 
+# I like my solution because it is similar to BFS and keeping track of values that can be
+# reached with # of coins used (the layers). Just need to make sure to keep track of 
+# seen to not bloat the queue.
+
+from typing import List
+from collections import deque
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        pass
+        if amount == 0:
+            return 0
+
+        queue = deque([amount])
+        seen = set()
+        coins_used = 0
+        while queue:
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                for j in coins:
+                    if curr-j == 0:
+                        return coins_used+1
+                    elif curr-j > 0 and curr-j not in seen:
+                        queue.append(curr-j)
+                        seen.add(curr-j)
+
+            #print(queue)
+            coins_used += 1
+
+        return -1
+
+        
--- neetcode_problems/1D_DynamicProgramming/M-House_Robber.py
+++ neetcode_problems/1D_DynamicProgramming/M-House_Robber.py
@@ -1,5 +1,14 @@
-# Hint:
+# Hint: Building left to right, can always track maximum solution
+# padding is not necessary but allows me to do a basic loop without worrying about
+# an array with only size of 1 or 2
 
+from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        pass
+        
+        nums = [0] + nums # add left pading for elegant solution
+        for i in range(2, len(nums)):
+            nums[i] = max(nums[i-1], nums[i-2] + nums[i])
+
+        return nums[-1]
+
