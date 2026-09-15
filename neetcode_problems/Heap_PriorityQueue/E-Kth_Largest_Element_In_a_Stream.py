db7d17739f7a5858536fb22a96d7cc6bab4e86f2
Luke Liang <liangl5@miamioh.edu>
2026-09-14 22:18:27 -0400

    heap / pqueue

--- neetcode_problems/Heap_PriorityQueue/E-Kth_Largest_Element_In_a_Stream.py
+++ neetcode_problems/Heap_PriorityQueue/E-Kth_Largest_Element_In_a_Stream.py
@@ -1,10 +1,21 @@
-# Hint:
+# Hint: largest means only need to keep track of k elements (can pop the rest)
 
+# need the final pop and boolean checkfor an edge case of empty and adding 1 element to it. 
+# if you wanted the k smallest it would be the same but with a max heap instead of a min heap
+
+from typing import List
 import heapq
-
 class KthLargest:
+
     def __init__(self, k: int, nums: List[int]):
-        pass
+        self.nums = nums
+        self.k = k
+        heapq.heapify(self.nums)
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
 
     def add(self, val: int) -> int:
-        pass
+        heapq.heappush(self.nums, val)
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
--- neetcode_problems/Heap_PriorityQueue/M-Task_Scheduler.py
+++ neetcode_problems/Heap_PriorityQueue/M-Task_Scheduler.py
@@ -1,8 +1,34 @@
-# Hint:
+# Hint: track cooldowns with a queue and max heap to get the next task with the most remaining count.
 
+# task actually doesnt matter, track the value. 
+# queue for cooldowns is necessary
+
+from collections import deque, Counter
 import heapq
-from collections import Counter
-
+from typing import List
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        pass
+        count = Counter(tasks)
+        heap = [-cnt for cnt in count.values()]
+
+        heapq.heapify(heap)
+        #print(heap)
+
+
+        cooldown = deque() # -cnt, idletime
+
+        time = 0
+        while heap or cooldown:
+            time += 1
+
+            if not heap:
+                time = cooldown[0][1]
+            else:
+                cnt = 1 + heapq.heappop(heap)
+                if cnt:
+                    cooldown.append((cnt, time+n))
+
+            if cooldown and cooldown[0][1] == time:
+                heapq.heappush(heap, cooldown.popleft()[0])
+
+        return time
